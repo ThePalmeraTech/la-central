0994144954aa89a67beea56f82e2b8bb7812b3e2
Hanaley Mosley <hanaleymosley@palmeratech.com>
2023-09-07 14:58:43 -0500

    Magazine framework - Article and Comment models

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # Defines the root path route ("/")
+  root 'articles#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :articles do
+    resources :comments
+  end
+end
---
+++ test/controllers/commets_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class CommetsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
