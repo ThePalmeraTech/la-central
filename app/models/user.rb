ab5e3cb10867587594133a264f484876c0f68eec
Hanaley Mosley <hanaleymosley@palmeratech.com>
2023-09-11 00:43:43 -0500

    Devise implementations and User role setup, permissions to access elements in the views based on user role

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: [:reader, :publisher, :admin]
 end
